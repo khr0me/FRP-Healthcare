245cd0c2e6dd34f3742f0dbf2a9de64767562bec
khr0me <121874632+khr0me@users.noreply.github.com>
2024-10-11 00:12:26 +0200

    Add files via upload

--- hackaton_project/wsgi.py
+++ hackaton_project/wsgi.py
@@ -3,3 +3,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackaton_project.settings')
 application = get_wsgi_application()
+
+app = application
+
+
+
